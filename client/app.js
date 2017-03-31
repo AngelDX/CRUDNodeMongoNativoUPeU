74b4ef124d4b65a4272ba0b822cf6c96de38bad0
AngelDX <angel.condori@gmail.com>
2017-03-31 09:51:51 -0500

    enviando datos al servidor

--- client/app.js
+++ client/app.js
@@ -16,6 +16,23 @@ class Crud{
 
 	sendForm(){
 		alert("Capturado el Submit")
+		let form_data=new FormData();
+		form_data.append('nombre',$('#nombre').val())
+		form_data.append('edad',$('#edad').val())
+		$.ajax({
+			url: '../server/insertRegistro',
+			dataType:'json',
+			processData: false,
+			contentType: false,
+			data: form_data,
+			type: 'POST',
+			success: function(respuesta){
+
+			},
+			error:function(){
+				alert('Error en la comuncacion con el servidor');
+			}
+		})
 	}
 }
 
